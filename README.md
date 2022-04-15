f40373e773aeba95e5fe283ded36c78b9f634c46
Illarion Khlestov <khlyestovillarion@gmail.com>
2022-04-16 02:31:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a smiple web server used for demo purposes
+
+# Installation
+
+- Create python virtual environment with `python3 -m venv .venv`
+- Activate environment with `source .venv/bin/activate`
+- Install required libraries `pip install -r requirements.txt`
---
+++ main.py
@@ -0,0 +1,30 @@
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
+
+
+class IndexView(web.View):
+    @aiohttp_jinja2.template("index.jinja2")
+    async def get(self):
+        return {}
+
+    @aiohttp_jinja2.template("index.jinja2")
+    async def post(self):
+        data = await self.request.post()
+        return {"username": data["username"]}
+
+
+def build_app():
+    app = web.Application()
+    aiohttp_jinja2.setup(
+        app,
+        loader=jinja2.FileSystemLoader("templates"),
+    )
+    app.add_routes([
+        web.view("/", IndexView),
+    ])
+    return app
+
+
+if __name__ == "__main__":
+    web.run_app(build_app())
